05e17c93665a10af5ec26d150c660645f5c6ef06
lm2024 <lm.2024@outlook.com>
2026-06-24 08:17:16 +0800

    删除多余文件

--- .history/backend/src/main/java/com/autotest/service/DictCategoryService_20260616164117.java
+++
@@ -1,9 +0,0 @@
-package com.autotest.service;
-
-import com.autotest.model.dto.dict.DictCategoryCreateDTO;
-import com.autotest.model.entity.DictCategory;
-
-import java.util.List;
-
-public interface DictCategoryService {
-    DictCategory createCategory(DictCategoryCreateDTO dto);
